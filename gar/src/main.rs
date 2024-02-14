ec3dcc18a7415af3cdd48e66576647e9ea3015ea
ayoub.laamim <ayoublaamim1996@gmail.com>
2024-02-14 20:08:51 +0100

    updated code

--- gar/src/main.rs
+++ gar/src/main.rs
@@ -1,3 +1,24 @@
+use std::{error::Error, io, process, fs::File};
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let file = File::open("data.csv").expect("Error opening file");
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here..
+        let record = result?;
+        for v in &record {
+            println!("{}", v);
+        }
+   
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
